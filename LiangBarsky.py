a84891fea0e6b74384a960f32a2cc43b805ec19f
Macbook <macbook@Macbook-5.local>
2018-09-03 11:15:22 -0500

    Agregando excepciones,cerrando el archivo

--- LiangBarsky.py
+++ LiangBarsky.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 verticeInf = []
 verticeSup = []
 
@@ -12,21 +12,25 @@ verticeSup = []
 #vertices[3] = pySup
 def LiangBarsky(pRef,vertices,modo): 
     u = 0
-    if (modo == True):
-        u = (pRef - vertices[0]) / (vertices[2] - vertices[0])
-        if (u < 0 or u > 1):
-            return False
-        y = vertices[1] + u * (vertices[3] - vertices[1])
-        punto = [pRef , y]
-        return punto
-    
-    elif (modo == False):
-        u = (pRef - vertices[1]) / (vertices[3] - vertices[1])
-        if (u < 0 or u > 1):
-            return False
-        x = vertices[0] + u * (vertices[2] - vertices[0])
-        punto = [x , pRef]
-        return punto
+    try:
+        if (modo == True):
+            u = (pRef - vertices[0]) / (vertices[2] - vertices[0])
+            if (u < 0 or u > 1):
+                return False
+            y = vertices[1] + u * (vertices[3] - vertices[1])
+            punto = [pRef , y]
+            return punto
+        
+        elif (modo == False):
+            u = (pRef - vertices[1]) / (vertices[3] - vertices[1])
+            if (u < 0 or u > 1):
+                return False
+            x = vertices[0] + u * (vertices[2] - vertices[0])
+            punto = [x , pRef]
+            return punto
+    except:
+        return False
+
 
 def validacion(punto):
     
@@ -43,22 +47,29 @@ def validacion(punto):
 
 
 while True:
-    verticeInf = input("Introduce las coordenadas del vertice inferior izquierdo: ").split(",")
-    verticeInf = list(map(int,verticeInf))
+    try:
+        verticeInf = input("Introduce las coordenadas del vertice inferior izquierdo: ").split(",")
+        verticeInf = list(map(int,verticeInf))
+        
+        if(len(verticeInf)!=2):
+            raise ValueError
     
-    if(len(verticeInf)!=2):
-        print("**Porfavor introduce las coordenadas separadas por comas, ejemplo 1,1. Asegurate que sean dos y que sean numeros enteros.")
+    except:
+        print("\n**Porfavor introduce las coordenadas separadas por comas, ejemplo 1,1. Asegurate que sean dos y que sean numeros enteros.\n")
         continue
-    
     print(verticeInf)
     break
 
 while True:
-    size = input("Introduce el ancho y alto de la ventana separado por comas: ").split(",")
-    size = list(map(int,size))
+    try:
+        size = input("Introduce el ancho y alto de la ventana separado por comas: ").split(",")
+        size = list(map(int,size))
 
-    if(len(size)!=2):
-        print("**Porfavor introduce el ancho y alto separado por comas, ejemplo 300,200. Asegurate que sean dos valores y que sean numeros enteros.")
+        if(len(size)!=2):
+            raise ValueError
+    
+    except:
+        print("\n**Porfavor introduce el ancho y alto separado por comas, ejemplo 300,200. Asegurate que sean dos valores y que sean numeros enteros.\n")
         continue
 
     verticeSup =[verticeInf[0]+size[0],verticeInf[1]+size[1]]
@@ -70,16 +81,19 @@ while True:
 
 archivo = open("lineas.txt","r")
 archivo_leido = archivo.read().split("\n")
+archivo.close()
 print(archivo_leido)
 lineas = []
 for linea_s in archivo_leido:
     linea_i=linea_s.split(" ")
     lineas.append(list(map(int,linea_i)))
-
+i=1
 for linea in lineas:
+    
+    print(f"Linea {i}")
     print(LiangBarsky(verticeInf[0],linea,True))
     print(LiangBarsky(verticeSup[0],linea,True))
     print(LiangBarsky(verticeInf[1],linea,False))
     print(LiangBarsky(verticeSup[1],linea,False))
-
+    i=i+1
     
