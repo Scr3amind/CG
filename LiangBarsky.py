a719ff0ff9ff95a8dff2883583fa889a5aa001ad
Isaac <screamindarko@gmail.com>
2018-09-05 15:33:53 -0500

    Versión final, arreglados bugs, cumple los requisitos de tarea

--- LiangBarsky.py
+++ LiangBarsky.py
@@ -1,99 +1,136 @@
 # -*- coding: utf-8 -*-
-verticeInf = []
-verticeSup = []
+'''
+Cruz Santos Isaac
+Computación Gráfica
+Grupo 04
+'''
 
+'''
+Función de el algoritmo de LiangBarsky Visto en clase
+modo == True -> Limites horizontales
+modo = False -> Limites Verticales
+vertices[0] = pxInf
+vertices[1] = pyInf
+vertices[2] = pxSup
+vertices[3] = pySup
+'''
 
-#Función de el algoritmo de LiangBarsky Visto en clase
-#modo == True -> Limites horizontales
-#modo = False -> Limites Verticales
-#vertices[0] = pxInf
-#vertices[1] = pyInf
-#vertices[2] = pxSup
-#vertices[3] = pySup
-def LiangBarsky(pRef,vertices,modo): 
+def LiangBarsky(pRef, vertices, modo):
     u = 0
     try:
-        if (modo == True):
+        # Modo horizontal
+        if (modo):
             u = (pRef - vertices[0]) / (vertices[2] - vertices[0])
             if (u < 0 or u > 1):
                 return False
             y = vertices[1] + u * (vertices[3] - vertices[1])
-            punto = [pRef , y]
-            return punto
-        
-        elif (modo == False):
+            punto = [pRef, round(y, 2)]
+
+            if(validacion(punto)):
+                return punto
+            else:
+                return False
+        # Modo Vetical
+        elif (modo is False):
             u = (pRef - vertices[1]) / (vertices[3] - vertices[1])
             if (u < 0 or u > 1):
                 return False
             x = vertices[0] + u * (vertices[2] - vertices[0])
-            punto = [x , pRef]
-            return punto
-    except:
+            punto = [round(x, 2), pRef]
+            if(validacion(punto)):
+                return punto
+            else:
+                return False
+    except BaseException:
         return False
+
+# Validacion del punto
 
 
 def validacion(punto):
-    
-    if(punto==False):
-        return False
-    if ((punto[0] >= verticeInf[0] and punto[0] <= verticeSup[0]) and (punto[1] >= verticeInf[1] and punto[1] <= verticeSup[1])):
+
+    if ((punto[0] >= verticeInf[0] and punto[0] <= verticeSup[0]) and (
+            punto[1] >= verticeInf[1] and punto[1] <= verticeSup[1])):
         return True
     else:
         return False
+# Impresión de puntos validos
 
 
-    
+def impPuntos(limite, punto):
+    print(f"\tCon limite {limite}: ", end='')
+    if(punto):
+        print(f"Punto de recorte en: {punto} ")
+    else:
+        print("No tiene punto de recorte")
 
 
+# Tomando entradas de ventana
+verticeInf = []
+verticeSup = []
 
 while True:
     try:
-        verticeInf = input("Introduce las coordenadas del vertice inferior izquierdo: ").split(",")
-        verticeInf = list(map(int,verticeInf))
-        
-        if(len(verticeInf)!=2):
+        verticeInf = input(
+            "Introduce las coordenadas del vertice inferior izquierdo: ").split(",")
+        verticeInf = list(map(int, verticeInf))
+
+        if(len(verticeInf) != 2):
             raise ValueError
-    
-    except:
+
+    except BaseException:
         print("\n**Porfavor introduce las coordenadas separadas por comas, ejemplo 1,1. Asegurate que sean dos y que sean numeros enteros.\n")
         continue
-    print(verticeInf)
+
     break
 
 while True:
     try:
-        size = input("Introduce el ancho y alto de la ventana separado por comas: ").split(",")
-        size = list(map(int,size))
+        size = input(
+            "Introduce el ancho y alto de la ventana separado por comas: ").split(",")
+        size = list(map(int, size))
 
-        if(len(size)!=2):
+        if(len(size) != 2):
             raise ValueError
-    
-    except:
+
+    except BaseException:
         print("\n**Porfavor introduce el ancho y alto separado por comas, ejemplo 300,200. Asegurate que sean dos valores y que sean numeros enteros.\n")
         continue
 
-    verticeSup =[verticeInf[0]+size[0],verticeInf[1]+size[1]]
-    
-    print(size)
-    print(verticeSup)
+    verticeSup = [verticeInf[0] + size[0], verticeInf[1] + size[1]]
+
     break
 
+# Imprimiendo Datos de ventana
+print("\nDatos de Ventana: ")
+print(f"Tamaño de ventana: {size[0]} x {size[1]}")
+print("Vertice inferior izquierdo: ", verticeInf)
+print("Vertice superior derecho: ", verticeSup)
 
-archivo = open("lineas.txt","r")
+# Abriendo el archivo en modo lectura
+archivo = open("lineas.txt", "r")
 archivo_leido = archivo.read().split("\n")
 archivo.close()
-print(archivo_leido)
+
+# Casteando datos a enteros
 lineas = []
 for linea_s in archivo_leido:
-    linea_i=linea_s.split(" ")
-    lineas.append(list(map(int,linea_i)))
-i=1
-for linea in lineas:
-    
-    print(f"Linea {i}")
-    print(LiangBarsky(verticeInf[0],linea,True))
-    print(LiangBarsky(verticeSup[0],linea,True))
-    print(LiangBarsky(verticeInf[1],linea,False))
-    print(LiangBarsky(verticeSup[1],linea,False))
-    i=i+1
-    
+    linea_i = linea_s.split(" ")
+    lineas.append(list(map(int, linea_i)))
+
+# Imprimiendo puntos de recorte
+for indice, linea in enumerate(lineas, start=1):
+
+    print(f"\nPara la linea {indice}:")
+
+    pIzq = LiangBarsky(verticeInf[0], linea, True)
+    impPuntos("izquierdo", pIzq)
+
+    pDer = LiangBarsky(verticeSup[0], linea, True)
+    impPuntos("derecho", pDer)
+
+    pInf = LiangBarsky(verticeInf[1], linea, False)
+    impPuntos("inferior", pInf)
+
+    pSup = LiangBarsky(verticeSup[1], linea, False)
+    impPuntos("superior", pSup)
